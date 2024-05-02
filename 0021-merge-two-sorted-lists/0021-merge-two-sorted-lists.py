483ad525d80e6956304e8c852159c3769cfc765c
AbhiBora28 <168160434+AbhiBora28@users.noreply.github.com>
2024-05-02 13:23:53 +0530

    Time: 36 ms (72.66%), Space: 16.6 MB (58.68%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -0,0 +1,44 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        current = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        
+        
+        current.next = l1 if l1 else l2
+        
+        return dummy.next
+
+
+solution = Solution()
+
+
+list1 = ListNode(1)
+list1.next = ListNode(2)
+list1.next.next = ListNode(4)
+
+list2 = ListNode(1)
+list2.next = ListNode(3)
+list2.next.next = ListNode(4)
+
+
+merged_list = solution.mergeTwoLists(list1, list2)
+
+
+while merged_list:
+    print(merged_list.val, end=" -> ")
+    merged_list = merged_list.next
