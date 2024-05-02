457cb8473bb6e5745e4b3935eb47912618617a41
AbhiBora28 <168160434+AbhiBora28@users.noreply.github.com>
2024-05-02 15:14:39 +0530

    Time: 40 ms (53.05%), Space: 16.7 MB (24.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,7 +7,7 @@ class Solution:
         j = n - 1
         k = m + n - 1
         
-        # Merge nums1 and nums2 into nums1
+       
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -17,19 +17,12 @@ class Solution:
                 j -= 1
             k -= 1
         
-        # If there are remaining elements in nums2, copy them to nums1
+        
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
 
-# Example usage:
-solution = Solution()
-nums1 = [1, 2, 3, 0, 0, 0]
-m = 3
-nums2 = [2, 5, 6]
-n = 3
-solution.merge(nums1, m, nums2, n)
-print(nums1)  # Output: [1, 2, 2, 3, 5, 6]
+
 
         
